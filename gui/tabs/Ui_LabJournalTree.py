fb091243dc6aafd10f5477a7059488da463866d5
Michael King <michael.king@uni-konstanz.de>
2017-11-21 11:34:02 +0100

    reordered files

--- gui/tabs/Ui_LabJournalTree.py
+++ gui/tabs/Ui_LabJournalTree.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Ui_tab_LabJournalTree.ui'
+# Form implementation generated from reading ui file 'Ui_LabJournalTree.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
