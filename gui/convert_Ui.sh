dafe5ac6da5a5fa2ce5a8dc6b8480b86803424a0
Michael King <michael.king@uni-konstanz.de>
2017-11-21 11:36:15 +0100

    reordered files

--- gui/convert_Ui.sh
+++ gui/convert_Ui.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # convert Ui_MainWindow
-pyuic4 Ui_MainWindow.ui -o GUi_MainWindow.py
+pyuic4 MainWindow.ui -o MainWindow.py
 
 
 pushd tabs
     # convert Ui_LabJournalTree
-    pyuic4 Ui_LabJournalTree.ui -o GUi_LabJournalTree.py
+    pyuic4 Ui_LabJournalTree.ui -o LabJournalTree.py
 
     pushd InfoEntry
         # convert Ui_InfoEntry
-        pyuic4 Ui_InfoEntry.ui -o GUi_InfoEntry.py
+        pyuic4 Ui_InfoEntry.ui -o InfoEntry.py
 
     popd # InfoEntry
 
