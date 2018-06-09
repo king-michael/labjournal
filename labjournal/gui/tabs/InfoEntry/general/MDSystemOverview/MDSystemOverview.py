0a7e933fbfc628ad69b40dd62c1dbb0db7bb46dd
Michael King <michael.king@uni-konstanz.de>
2018-06-09 14:00:27 +0200

    small fix

--- labjournal/gui/tabs/InfoEntry/general/MDSystemOverview/MDSystemOverview.py
+++ labjournal/gui/tabs/InfoEntry/general/MDSystemOverview/MDSystemOverview.py
@@ -5,9 +5,11 @@ Widget to show details about the system
 import os
 import pkg_resources
 import logging
+import MDAnalysis
 
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 from Ui_MDSystemOverview import Ui_Form
 from pymolwidget import PyMolWidget
 from glob import glob
@@ -25,7 +27,11 @@ class MDSystemOverview(QWidget,Ui_Form):
 
         self.setup_variables()  # setup some variables
 
-        self.setup_display()
+        # add a pushButton to show the structure
+        self.pushButtonShow = QPushButton("show structure")
+        self.pushButtonShow.pressed.connect(self.event_press_show)
+        self.layout_display.addWidget(self.pushButtonShow)
+        #self.setup_display()
 
         self.setup_details()
 
@@ -38,6 +44,15 @@ class MDSystemOverview(QWidget,Ui_Form):
         else:
             self.pdb_file = [os.path.basename(f) for f in list_pdbfiles]
             raise NotImplementedError("Not implemented more then one pdb file")
+
+    def event_press_show(self):
+        """
+        Event which is called to display the pymolwidget
+        """
+        print(self.layout_display.removeWidget(self.pushButtonShow))
+        self.pushButtonShow.deleteLater()
+        # show the display
+        self.setup_display()
 
     def setup_display(self):
         """
@@ -57,13 +72,45 @@ class MDSystemOverview(QWidget,Ui_Form):
         else: # Todo: Implement several pdb files if found
             return
 
+
     def setup_details(self):
         """
         Setup the details
         """
 
-        pass
 
+
+
+        self.infoText = QLabel()
+        if self.pdb_file is not None:
+            u = MDAnalysis.Universe(os.path.join(self.path,self.pdb_file))
+            ts = u.trajectory[0]
+
+            # add the total number of atoms
+            n_atoms= u.atoms.n_atoms
+            text = "N(total)   : {}\n".format(n_atoms)
+
+            # if we find a protein add it
+            n_atoms_protein = u.select_atoms('protein').n_atoms
+            if n_atoms_protein != 0:
+                text += "N(protein) : {}\n".format(n_atoms_protein)
+
+            # if we find n_atoms_solvent add the
+            resname_water = ["SOL", "WATER", "SOLV", "mW", "H2O"]
+            cond_water = " or ".join(["resname {}".format(i) for i in resname_water])
+            n_atoms_solvent = u.select_atoms(cond_water).n_atoms
+            if n_atoms_solvent != 0:
+                text+= "N(solvent) : {}\n".format(n_atoms_solvent)
+
+            # add box info
+            text+= "\nBox (a,b,c, alpha, beta, gamma):\n{}".format(ts.dimensions)
+        else:
+            text="No pdb file found!"
+
+        self.infoText.setText(text)
+        self.infoText.setAlignment(Qt.AlignTop)
+        self.layout_details.addWidget(self.infoText)
+        self.infoText.setFont(QFont("Monospace"))
 
 if __name__ == '__main__':
     import sys
